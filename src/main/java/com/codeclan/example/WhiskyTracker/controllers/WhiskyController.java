8652c0211241b1027cd6b0c2df7ceea99b1f9b64
sierratango34 <k.sritawan@gmail.com>
2019-05-22 17:27:40 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,31 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/whiskies")
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/year/{year}")
+    public List<Whisky> findWhiskyByYear(@PathVariable int year){
+        return whiskyRepository.findWhiskyByYear(year);
+    }
+
+    @GetMapping(value = "/distillery/{distillery}/age/{age}")
+    public List<Whisky> getWhiskyOfParticularDistilleryAndParticularAge(@PathVariable String distillery, Integer age){
+        return whiskyRepository.getWhiskyOfParticularDistilleryAndParticularAge(distillery, age);
+    }
+
+}
